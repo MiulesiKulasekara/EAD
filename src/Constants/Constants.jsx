4f59f97c5b64336143565b3d4573d9e284ce5a8a
MiulesiKulasekara <it21028878@my.sliit.lk>
2024-10-01 18:17:15 +0530

    add constants

---
+++ src/Constants/Constants.jsx
@@ -0,0 +1,23 @@
+// LumaShopEnums.js
+
+export const UserRoleConstant = {
+    CUSTOMER: 1,
+    VENDOR: 2,
+    ADMIN: 3,
+    CSR: 4
+  };
+  
+  export const UserStatusConstant = {
+    ACTIVE: 1,
+    INACTIVE: 2,
+    PENDING: 3
+  };
+  
+  export const OrderStatusEnum = {
+    PENDING: 5,
+    INPROGRESS: 10,
+    DELIVERED: 15,
+    CANCELLED: 20,
+    COMPLETED: 25
+  };
+  
